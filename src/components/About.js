879e54f8c8326d9bde0383392fc1d703a4261031
Deepakraj125 <deepak65521@gmail.com>
2022-06-01 19:10:55 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div className="container">
+        <div className="about">
+            <p>HPE is the global edge-to-cloud company built to transform your business. How? By helping you connect, protect, analyze, and act on all your data and applications wherever they live, from edge to cloud, so you can turn insights into outcomes at the speed required to thrive in today’s complex world.</p>
+        </div>
+
+        {/* <div className="vision">
+        "At HPE, we’re ushering in a new era: the Age of Insight, where we make all your data work for you, wherever it lives. This era will lead to services and products we can’t yet imagine, and it will generate discoveries that elevate the greater well-being of every human on this planet."
+        </div> */}
+        <div>
+          <h3> Presenting  AI based Chatbot for Costumer support</h3>
+        </div>
+
+        <div className="goal"> At HPE, our team members search beyond customers' needs today to accelerate what’s next and make a difference—for others, our company, and the planet.</div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+// using named export 
+export function Navbar(props){
+
+    return (
+        <div className="navbar">
+            <div>Home</div>
+            <div>News</div>
+            <div>Contact</div>
+            <div>About</div>
+        
+        </div>
+        
+        
+    )
+};
